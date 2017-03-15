b68572e2239aaaae7ba4b3266e6d97c85c03dec7
customgame <customgame@users.noreply.github.com>
2017-03-16 03:18:37 +0800

    Added BS - for testing Sensor node 2.0
    This program is for testing Sensor node 2.0

---
+++ Arduino/BS_-_for_testing_Sensor_node_2.0/BS_-_for_testing_Sensor_node_2.0.ino
@@ -0,0 +1,44 @@
+unsigned long prevMillis=0;
+bool zigbeeDataAvailable=false;
+String zigbeeData="";
+
+void setup() {
+  Serial.begin(57600);
+  Serial2.begin(38400);
+}
+
+void loop() {
+  unsigned long currentMillis;
+  
+  if(zigbeeDataAvailable)
+  {
+    Serial.println(zigbeeData);
+    zigbeeData="";
+    zigbeeDataAvailable=false;
+  }
+
+  currentMillis=millis();
+  if(currentMillis-prevMillis>999)
+  {
+    Serial2.print("#A*");  
+    prevMillis=currentMillis;
+  }
+}
+
+void serialEvent2()
+{
+  char in;
+  if (Serial2.available() > 0)
+  {
+    in = (char)Serial2.read();
+    zigbeeData += in;
+    if ((in == '*') && (zigbeeData.length() == 4))
+    {
+      zigbeeDataAvailable = true;
+    }
+    else if ((in == '*') && (zigbeeData.length() != 4))
+    {
+      zigbeeData = "";
+    }
+  }
+}
